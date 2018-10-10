78ca0b91af0c381cd01dd4f3b41c80e42046af5a
Laptop\Roger <roger.birkenes+github@gmail.com>
2018-10-11 01:35:41 +0200

    Begynt på administratordel

---
+++ Controllers/AdminController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Graubakken_Filmsjappe.Controllers
+{
+    public class AdminController : Controller
+    {
+        // GET: Admin
+        public ActionResult Admin()
+        {
+            if (AdminLoggetInn())
+            {
+                return View();
+            }
+            return RedirectToAction("AdminLoginn");
+        }
+
+        public bool AdminLoggetInn()
+        {
+            return Session["Admin"] != null && (string)Session["Admin"] != "";
+        }
+
+        public ActionResult AdminLoginn()
+        {
+            return View();
+        }
+    }
+}
---
+++ Models/Administrator.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Graubakken_Filmsjappe.Models
+{
+    public class Administrator
+    {
+        public int id { get; set; }
+        public string Brukernavn { get; set; }
+        public string Passord { get; set; }
+    }
+
+    public class AdministratorDB
+    {
+        public int id { get; set; }
+        public string Brukernavn { get; set; }
+        public byte[] Passord { get; set; }
+        public string Salt { get; set; }
+    }
+}
